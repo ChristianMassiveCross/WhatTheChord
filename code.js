27d00b48f052a671aef56055f4e2893c5a02e1c8
ChristianMassiveCross <Christianbreitkreutz@gmx.de>
2012-07-05 20:50:17 +0200

    ankes aenderung

--- code.js
+++
@@ -1,7 +0,0 @@
-// init
-// schreibe akord auf
-// hole noten aus feldern
-// wandle Noten in zahlen
-// überprüfe die Noten aus den felder auf Chord
-// überprüfe die Noten aus den felder auf inversion nummer
-// gib ergibnisse aus
